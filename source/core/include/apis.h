a09a394727fd44895d855ad5c37fdb4a320a2a19
noxabellus <noxabellus@gmail.com>
2019-05-10 16:48:43 -0500

    Added MODULE_INTERNAL for C++ style module exports

--- source/core/include/apis.h
+++ source/core/include/apis.h
@@ -11,6 +11,7 @@
     #define ENGINE_API __declspec(dllexport)
     #define GAME_API __declspec(dllimport)
     #define MODULE_API extern "C" __declspec(dllimport)
+    #define MODULE_INTERNAL __declspec(dllimport)
   #elif defined(M_GAME)
     #if defined(M_ENGINE) || defined(M_MODULE)
       #error Only one of M_ENGINE, M_GAME, or M_MODULE may be defined while compiling
@@ -18,6 +19,7 @@
     #define ENGINE_API __declspec(dllimport)
     #define GAME_API __declspec(dllexport)
     #define MODULE_API extern "C" __declspec(dllimport)
+    #define MODULE_INTERNAL __declspec(dllimport)
   #elif defined(M_MODULE)
     #if defined(M_ENGINE) || defined(M_GAME)
       #error Only one of M_ENGINE, M_GAME, or M_MODULE may be defined while compiling
@@ -25,10 +27,12 @@
     #define ENGINE_API __declspec(dllimport)
     #define GAME_API __declspec(dllimport)
     #define MODULE_API extern "C" __declspec(dllexport)
+    #define MODULE_INTERNAL __declspec(dllexport)
   #elif defined(__INTELLISENSE__)
     #define ENGINE_API
     #define GAME_API
     #define MODULE_API extern "C"
+    #define MODULE_INTERNAL
   #else
     #error One of either M_ENGINE, M_GAME, or M_MODULE must be defined while compiling
   #endif
@@ -37,6 +41,7 @@
   #define ENGINE_API
   #define GAME_API
   #define MODULE_API extern "C"
+  #define MODULE_INTERNAL
 #endif
 
 #endif
